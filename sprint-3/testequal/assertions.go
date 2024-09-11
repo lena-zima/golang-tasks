e09e008ef2078f2ca498a3ab3589ca4e428b5887
Elena <elena.gibadullina@netcracker.com>
2024-09-12 02:04:21 +0300

    Sprint 3 commited

--- sprint-3/testequal/assertions.go
+++ sprint-3/testequal/assertions.go
@@ -2,13 +2,45 @@
 
 package testequal
 
+import(
+	"reflect"
+)
+
+func IsEqual(expected, actual interface{}) bool{
+	/*if reflect.ValueOf(expected).Type() != reflect.ValueOf(actual).Type(){
+		return false
+	}*/
+
+	switch expected.(type) {
+	case []int, []byte, map[string]string, string:
+		return reflect.DeepEqual(expected, actual)
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return expected == actual
+	default:
+		return false
+	}
+}
+	
+
 // AssertEqual checks that expected and actual are equal.
 //
 // Marks caller function as having failed but continues execution.
 //
 // Returns true iff arguments are equal.
 func AssertEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) bool {
-	panic("implement me")
+	t.Helper()
+
+	if IsEqual(expected,actual) {
+		return true
+	}
+
+	if len(msgAndArgs) != 0{
+		t.Errorf(msgAndArgs[0].(string), msgAndArgs[1:]...)
+	}else {
+		t.Errorf("")
+	}
+
+	return false
 }
 
 // AssertNotEqual checks that expected and actual are not equal.
@@ -17,15 +49,52 @@ func AssertEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) b
 //
 // Returns true iff arguments are not equal.
 func AssertNotEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) bool {
-	panic("implement me")
+	t.Helper()
+
+	if !IsEqual(expected,actual) {
+		return true
+	}
+
+	if len(msgAndArgs) != 0{
+		t.Errorf(msgAndArgs[0].(string), msgAndArgs[1:]...)
+	}else {
+		t.Errorf("")
+	}
+
+	return false
 }
 
 // RequireEqual does the same as AssertEqual but fails caller test immediately.
 func RequireEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) {
-	panic("implement me")
+
+	t.Helper()
+
+	if IsEqual(expected,actual) {
+		return
+	}
+
+	if len(msgAndArgs) != 0{
+		t.Errorf(msgAndArgs[0].(string), msgAndArgs[1:]...)
+	}else {
+		t.Errorf("")
+	}
+
+	t.FailNow()
 }
 
 // RequireNotEqual does the same as AssertNotEqual but fails caller test immediately.
 func RequireNotEqual(t T, expected, actual interface{}, msgAndArgs ...interface{}) {
-	panic("implement me")
+	t.Helper()
+
+	if !IsEqual(expected,actual) {
+		return
+	}
+
+	if len(msgAndArgs) != 0{
+		t.Errorf(msgAndArgs[0].(string), msgAndArgs[1:]...)
+	}else {
+		t.Errorf("")
+	}
+
+	t.FailNow()
 }
